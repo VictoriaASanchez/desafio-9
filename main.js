20858b0fea47d3ccfd9aa5564d78e35563533807
Victoria Sánchez <vsanchezailen@gmail.com>
2023-12-21 17:39:38 -0300

    ADD changes

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-/* 
-1. Ver si se puede optimizar punto 3
-2. Terminar de darle estilo al botón (sombra, inset)
-*/
-
 /* --------------- PUNTO 1 --------------- */
 
-const character = window.prompt('¿Quién se presenta hoy? (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)');
+/* const character = window.prompt('¿Quién se presenta hoy? (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)');
 
-console.log(character);
+console.log(character); */
 
 /* --------------- PUNTO 2 --------------- */
 
-const span = document.querySelector('h1 > span');
+/* const span = document.querySelector('h1 > span');
 
 switch (character) {
     case "Mario":
@@ -42,10 +37,10 @@ switch (character) {
         console.log(span.textContent = "desconocido");
         break;
     }
-
+ */
 /* --------------- PUNTO 3 --------------- */
 
-const mario = document.getElementById('mario');
+/* const mario = document.getElementById('mario');
 const luigi = document.getElementById('luigi');
 const bowser = document.getElementById('bowser');
 const peach = document.getElementById('peach');
@@ -70,6 +65,83 @@ if (characterLowerCase == 'mario') {
     toad.title = "Presentado";
 } else {
     toadette.title = "Presentado";
-}
+} */
 
 /* --------------- PUNTO 6 --------------- */
+
+function presentar() {
+
+    /* --------------- PUNTO 1 --------------- */
+    
+    const character = window.prompt('¿Quién se presenta hoy? (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)');
+
+    console.log(character);
+
+    /* --------------- PUNTO 2 --------------- */
+
+    const span = document.querySelector('h1 > span');
+
+    switch (character.toLowerCase()) {
+        case "mario":
+            console.log(span.textContent = "Mario");
+            break;
+        case "luigi":   
+            console.log(span.textContent = "Luigi");
+            break;
+        case "bowser":
+            console.log(span.textContent = "Bowser Morton Koopa");
+            break;
+        case "peach":
+            console.log(span.textContent = "Princesa Peach Toadstool");
+            break;
+        case "yoshi":
+            console.log(span.textContent = "T. Yoshisaur Munchakoopas");
+            break;
+        case "toad":
+            console.log(span.textContent = "Toad");
+            break;
+        case "toadette":
+            console.log(span.textContent = "Toadette");
+            break;
+        case "daisy":
+            console.log(span.textContent = "Princesa Daisy");
+            break;
+        default:
+            console.log(span.textContent = "desconocido");
+            break;
+        }
+
+    /* --------------- PUNTO 3 --------------- */
+
+    const mario = document.getElementById('mario');
+    const luigi = document.getElementById('luigi');
+    const bowser = document.getElementById('bowser');
+    const peach = document.getElementById('peach');
+    const yoshi = document.getElementById('yoshi');
+    const toad = document.getElementById('toad');
+    const toadette = document.getElementById('toadette');
+
+    const characterLowerCase = character.toLowerCase();
+    console.log(characterLowerCase);
+
+    if (characterLowerCase == 'mario') {
+        mario.title = "Presentado";
+    } else if (characterLowerCase == 'luigi') {
+        luigi.title = "Presentado";
+    } else if (characterLowerCase == 'bowser') {
+        bowser.title = "Presentado";
+    } else if (characterLowerCase == 'peach') {
+        peach.title = "Presentado";
+    } else if (characterLowerCase == 'yoshi') {
+        yoshi.title = "Presentado";
+    } else if (characterLowerCase == 'toad') {
+        toad.title = "Presentado";
+    } else {
+        toadette.title = "Presentado";
+    }
+
+    /* --------------- BUTTON --------------- */
+
+    const btn = document.getElementById('btn');
+    btn.style.visibility = "hidden";
+}
